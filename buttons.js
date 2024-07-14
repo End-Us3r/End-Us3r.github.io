f4fe1958f51d6e81dea671b7fc8a72117cd3cb76
J.Atkins <89127977+End-Us3r@users.noreply.github.com>
2024-07-13 20:08:16 -0700

    Update 7.13.24
    Minor fixes, added javascript

---
+++ buttons.js
@@ -0,0 +1,25 @@
+function redirectToPage(url) {
+    window.location.href = url;
+}
+
+function setupButtonListeners() {
+    const buttons = {
+        button_one: 'https://end-us3r.github.io/Inked-Art-Homepage-Project/',
+        button_two: 'projects-folder/coffeeBot.html',
+        button_three: 'projects-folder/gpaCalculator.html',
+        button_four: 'projects-folder/boredlessTourist.html',
+        button_five: 'projects-folder/rps.html',
+        button_six: 'projects-folder/nightmareGame.html'
+    };
+
+    for (const buttonId in buttons) {
+        buttonElement = document.getElementById(buttonId);
+        if (buttonElement) {
+            buttonElement.addEventListener('click', () => {
+                redirectToPage(buttons[buttonId]);
+            });
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', setupButtonListeners);
